485c3982d82701c005f53765d4ffd544be573746
Zaechus <zaechus@proton.me>
2022-11-28 12:25:11 -0700

    remove unwanted default webpage behavior

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,10 @@
-use axum::{
-    http::StatusCode,
-    response::{Html, IntoResponse},
-    routing::{get, get_service},
-    Router,
-};
+use axum::{http::StatusCode, response::IntoResponse, routing::get_service, Router};
 use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
     let serve_dir = get_service(ServeDir::new("docs")).handle_error(handle_error);
-    let app = Router::new().route("/", get(index)).fallback(serve_dir);
+    let app = Router::new().fallback(serve_dir);
 
     axum::Server::bind(&"0.0.0.0:7878".parse().unwrap())
         .serve(app.into_make_service())
@@ -17,10 +12,6 @@ async fn main() {
         .unwrap();
 }
 
-async fn index() -> Html<&'static str> {
-    Html(include_str!("./index.html"))
-}
-
 async fn handle_error(_err: std::io::Error) -> impl IntoResponse {
-    (StatusCode::INTERNAL_SERVER_ERROR, "Something went wrong...")
+    StatusCode::INTERNAL_SERVER_ERROR
 }
